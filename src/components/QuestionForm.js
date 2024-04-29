a8cbe083f71110eec2e877352aa09badf383908e
James <jamonjoroge816@gmail.com>
2024-04-29 11:15:07 +0300

    DONE

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,3 +1,4 @@
+
 import React, { useState } from "react";
 
 function QuestionForm(props) {
@@ -19,8 +20,24 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: formData.prompt,
+        answers: [
+          formData.answer1,
+          formData.answer2,
+          formData.answer3,
+          formData.answer4,
+        ],
+        correctIndex: parseInt(formData.correctIndex),
+      }),
+    })
   }
+
 
   return (
     <section>
@@ -84,7 +101,7 @@ function QuestionForm(props) {
             <option value="3">{formData.answer4}</option>
           </select>
         </label>
-        <button type="submit">Add Question</button>
+        <button type="submit" >Add Question</button>
       </form>
     </section>
   );
